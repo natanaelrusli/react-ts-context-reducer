4bfcaa527370273bcc0164a73a8929ee74295e0e
Nata Nael <natanael280198@gmail.com>
2023-09-15 14:28:47 +0700

    fix(cartReducer): fix addToCart dispatch

--- src/reducers/cartReducer.tsx
+++ src/reducers/cartReducer.tsx
@@ -3,18 +3,19 @@ import { CartItf, ProductItf } from "../types";
 export type CartAction = { type: "addToCart"; payload: ProductItf };
 
 export function cartReducer(state: CartItf, action: CartAction): CartItf {
-  const existingProduct = state.products.find((product) => {
-    return product.id === action.payload.id;
-  });
+  const existingProduct = state.products.find(
+    (product) => product.id === action.payload.id
+  );
 
   switch (action.type) {
     case "addToCart":
       if (existingProduct) {
         return {
-          products: [
-            ...state.products,
-            { ...existingProduct, quantity: existingProduct.quantity + 1 },
-          ],
+          products: state.products.map((product) =>
+            product.id === action.payload.id
+              ? { ...product, quantity: product.quantity + 1 }
+              : product
+          ),
           totalPrice: state.totalPrice + action.payload.price,
         };
       } else {
